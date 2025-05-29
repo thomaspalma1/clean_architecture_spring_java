655766727ce615ff4fcbaaa7b17db9bc9c77a1d1
Thomas Palma <thomaspalma777@gmail.com>
2025-05-28 21:09:23 -0300

    [feature] add User domain entity
    - added fields: cpf, name, dateOfBirth, and email;
    - implemented getters and setters for all fields.

---
+++ src/main/java/br/com/alura/codechella/domain/User.java
@@ -0,0 +1,43 @@
+package br.com.alura.codechella.domain;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private String cpf;
+    private String name;
+    private LocalDate dateOfBirth;
+    private String email;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
