d3c679e10146a8fb864b8a6617dda756d2f0909b
Thomas Palma <thomaspalma777@gmail.com>
2025-07-19 21:54:32 -0300

    [feature] provide UserRepositoryJPA and UserEntityMapper beans in UserConfiguration
    - added `userRepository` bean method returning `UserRepositoryJPA`;
    - added `userEntityMapper` bean method returning `UserEntityMapper`.

--- src/main/java/br/com/alura/codechella/configuration/UserConfiguration.java
+++ src/main/java/br/com/alura/codechella/configuration/UserConfiguration.java
@@ -3,6 +3,9 @@ package br.com.alura.codechella.configuration;
 
 import br.com.alura.codechella.application.gateways.UserRepository;
 import br.com.alura.codechella.application.usecases.CreateUser;
+import br.com.alura.codechella.infrastructure.gateways.UserRepositoryJPA;
+import br.com.alura.codechella.infrastructure.gateways.UserEntityMapper;
+import br.com.alura.codechella.infrastructure.persistence.UserJpaRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +15,17 @@ public class UserConfiguration {
     @Bean
     CreateUser createUser(UserRepository userRepository){
         return new CreateUser(userRepository);
+    }
+
+    @Bean
+    UserRepository userRepository(
+            UserJpaRepository userJpaRepository,
+            UserEntityMapper userEntityMapper) {
+        return new UserRepositoryJPA(userJpaRepository, userEntityMapper);
+    }
+
+    @Bean
+    UserEntityMapper userEntityMapper(){
+        return new UserEntityMapper();
     }
 }
